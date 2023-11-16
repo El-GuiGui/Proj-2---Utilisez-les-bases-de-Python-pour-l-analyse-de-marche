cc0de28064fb9c8cd0439ff32772c7cb5052c5c1
Guillaume Lauroy <gui.lauroy@gmail.com>
2023-11-16 12:26:07 +0100

    all book in travel2

--- main.py
+++ main.py
@@ -7,59 +7,52 @@ import re
 from urllib.parse import urljoin
 
 
+# Fonction pour extraire les données de la page web
+def extraction(url):
+    reponse = requests.get(url)
+    # Définir l'encodage
+    reponse.encoding = "utf-8"
+    soup = BeautifulSoup(reponse.text, features="html.parser")
+    return soup
+
+
+# Fonction pour extraire les URLs des livres d'une catégorie
 def extraire_urls_livres(url_categorie):
     urls_livres = []
-    base_url = "https://books.toscrape.com/"  # URL de base pour le site
+    # URL de base avec /catalogue/
+    base_url = "https://books.toscrape.com/catalogue/"
 
     while True:
         soup = extraction(url_categorie)
         livres = soup.find_all("article", class_="product_pod")
         for livre in livres:
             lien = livre.find("h3").find("a")["href"]
-            lien_complet = urljoin(base_url, lien)  # Combinaison correcte de l'URL
+            lien_complet = base_url + lien.replace(
+                "../", ""
+            )  # Construction manuelle de l'URL
             urls_livres.append(lien_complet)
 
         next_button = soup.find("li", class_="next")
         if next_button:
             next_page = next_button.find("a")["href"]
-            url_categorie = urljoin(url_categorie, next_page)
+            url_categorie = urljoin(
+                "https://books.toscrape.com/catalogue/category/books/travel_2/",
+                next_page,
+            )
         else:
             break
 
     return urls_livres
 
 
-# Fonction pour nettoyer et transformer les textes
-def transformation_texte(texte):
-    texte = re.sub(r"^\s+|\s+$", "", texte)  # Enlever les espaces en début et en fin
-    texte = re.sub(r"\s+", " ", texte)  # Remplacer les espaces multiples
-    texte = re.sub(r'[^\w\s£€$.,;:!\'"()?-]', "", texte)  # Conserver certains symboles
-    return texte
-
-
-# Fonction pour transformer la disponibilité en un nombre simple
-def transformation_disponibilite(texte):
-    texte = texte.strip()  # Nettoyage de base du texte
-    match = re.search(r"\d+", texte)  # Extraire le nombre
-    return match.group() if match else texte
-
-
-# Fonction pour extraire les données de la page web
-def extraction(url):
-    reponse = requests.get(url)
-    reponse.encoding = "utf-8"  # Définir l'encodage
-    soup = BeautifulSoup(reponse.text, features="html.parser")
-    return soup
-
-
 # Fonction pour transformer les données extraites
 def transformation_tdp(soup, url_livre):
     product_main = soup.find("div", {"class": "col-sm-6 product_main"})
     if not product_main:
-        return [None] * 10  # Retourner une liste de None si la page n'est pas correcte
-
-    title = transformation_texte(product_main.find("h1").text)
+        # Retourner une liste de None si la page n'est pas correcte
+        return [None] * 10
     # Extraction et transformation du titre, de la description, du prix
+    title = transformation_texte(product_main.find("h1").text)
 
     description = transformation_texte(
         soup.find("article", {"class": "product_page"}).find("p", {"class": ""}).text
@@ -107,6 +100,26 @@ def transformation_tdp(soup, url_livre):
     ]
 
 
+# Fonction pour nettoyer et transformer les textes
+def transformation_texte(texte):
+    # Enlever les espaces en début et en fin
+    texte = re.sub(r"^\s+|\s+$", "", texte)
+    # Remplacer les espaces multiples
+    texte = re.sub(r"\s+", " ", texte)
+    # Conserver certains symboles
+    texte = re.sub(r'[^\w\s£€$.,;:!\'"()?-]', "", texte)
+    return texte
+
+
+# Fonction pour transformer la disponibilité en un nombre simple
+def transformation_disponibilite(texte):
+    # Nettoyage de base du texte
+    texte = texte.strip()
+    # Extraire le nombre
+    match = re.search(r"\d+", texte)
+    return match.group() if match else texte
+
+
 # Fonction pour sauvegarder les données et les images
 def sauvegarde(donnee, soup, en_tete, nom_fichier_csv="data.csv", folder_name="images"):
     mode = "a" if os.path.exists(nom_fichier_csv) else "w"
@@ -138,36 +151,7 @@ def traiter_et_sauvegarder(url_livre):
     sauvegarde(donnee, soup, en_tete, "data.csv", "images")
 
 
-# Fonction pour extraire les URLs des livres d'une catégorie
-def extraire_urls_livres(url_categorie):
-    urls_livres = []
-    base_url = "https://books.toscrape.com/catalogue/"  # URL de base avec /catalogue/
-
-    while True:
-        soup = extraction(url_categorie)
-        livres = soup.find_all("article", class_="product_pod")
-        for livre in livres:
-            lien = livre.find("h3").find("a")["href"]
-            lien_complet = base_url + lien.replace(
-                "../", ""
-            )  # Construction manuelle de l'URL
-            urls_livres.append(lien_complet)
-
-        next_button = soup.find("li", class_="next")
-        if next_button:
-            next_page = next_button.find("a")["href"]
-            url_categorie = urljoin(
-                "https://books.toscrape.com/catalogue/category/books/travel_2/",
-                next_page,
-            )
-        else:
-            break
-
-    return urls_livres
-
-
 # Traiter et sauvegarder les données d'un livre
-
 en_tete = [
     "titre",
     "description",
